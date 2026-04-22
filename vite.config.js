7be3bb9098eb515af2717f4e1a50f1b0df72776e
rahman2008-svg <prince.ar.abdur.rahman2008@gmail.com>
2026-04-22 12:52:15 +0600

    fix vite allowedHosts render issue

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
-  },
   preview: {
     host: true,
-    port: 10000
+    allowedHosts: true
   }
 })
